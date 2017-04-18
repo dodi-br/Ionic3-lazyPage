b2cd90e8ec3647a7aa23f79b3d41189b0f9e331e
Wong <123899696@qq.com>
2017-04-19 05:32:35 +0800

    shouye

--- pages/home-page/home-page.ts
+++ pages/home-page/home-page.ts
@@ -15,8 +15,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home-page.html',
 })
 export class HomePage {
+  childPage = {
+    cardRechargePage : { title: '充值', page: 'CardRechargePage' },
+    noticePage: { title: '公告', page: 'NoticePage' },
+    mapPage: { title: '地图', page: 'MapPage' },
+    storeDetailPage: { title: '店铺详情', page: 'StoreDetailPage' },
+    loginSignupPage: { page: 'LoginSignupPage' },  // Login/signup or person information page
+    personInformationPage: { page: 'PersonInformationPage' },
+    searchStorePage: { page: 'SearchStorePage' }
+  }
+  position = {
+    latitude: 116.39,
+    longtitude: 39.9,
+    distance: 500
+  }
+
+  // keys = Object.keys(this.childPage);
+  isLogin: false;
+  rankTab: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.rankTab = 'game1';
   }
 
   ionViewDidLoad() {
--- pages/store-page/store-page.ts
+++ pages/store-page/store-page.ts
@@ -15,7 +15,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'store-page.html',
 })
 export class StorePage {
-
+  store = [
+    {
+      label: "理想咖啡店",
+      address: "东兴路228号",
+      latitude: "234.22",
+      longtitude: "28.2",
+      distance: 318,
+      averageCost: 8.6,
+      phone: '15988152673',
+      email: '123899696@qq.com',
+      room: false,
+      parking: true,
+      credit: false,
+      operationTime: '9:00 - 18:00',
+      picture: "assets/shop1.jpg"
+    },
+    {
+      label: "喜马拉店",
+      address: "江南大道783",
+      latitude: "23",
+      longtitude: "2.2",
+      distance: 50,
+      averageCost: 5.6,
+      phone: '15988152673',
+      email: '123899696@qq.com',
+      room: false,
+      parking: true,
+      credit: false,
+      operationTime: '9:00 - 18:00',
+      picture: "assets/Shop2.jpg"
+    }
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
